83049448eb410c249bc7cd37c2b42ec719c78ea2
Heng Sophavong <hengsophavong@gmail.com>
2024-08-06 00:03:54 +0700

    typo error again

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -8,26 +8,30 @@ import Form from "@components/Form";
 const UpdatePrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-    const promptId = searchParams.get("id");
+  const promptId = searchParams.get("id");
 
+  const [post, setPost] = useState({ prompt: "", tag: "", });
   const [submitting, setIsSubmitting] = useState(false);
-  const [post, setPost] = useState({ prompt: "", tag: "" });
 
   useEffect(() => {
     const getPromptDetails = async () => {
       const response = await fetch(`/api/prompt/${promptId}`);
-        const data = await response.json();
+      const data = await response.json();
 
-        setPost({ prompt: data.prompt, tag: data.tag });
-    }
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+
     if (promptId) getPromptDetails();
   }, [promptId]);
 
-  const UpdatePrompt = async (e) => {
+  const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
 
-    if (!promptId) return alert("Prompt ID Not Found");
+    if (!promptId) return alert("Missing PromptId!");
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -54,7 +58,7 @@ const UpdatePrompt = () => {
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={UpdatePrompt}
+      handleSubmit={updatePrompt}
     />
   );
 };
